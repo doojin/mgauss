20e1024d1e39b796e50259afcba1a927080968fa
doojin <dmitry.papka@gmail.com>
2014-12-12 10:26:47 -0200

    abs() method added

--- vector.go
+++ vector.go
@@ -1,5 +1,7 @@
 package mgauss
 
+import "math"
+
 // Vector stores float numbers and provides methods to manipulate them
 type Vector struct {
 	values []float64
@@ -14,4 +16,12 @@ func NewVector(values []float64) (v Vector) {
 // Get returns vector's element by it's index
 func (v Vector) Get(index int) float64 {
 	return v.values[index]
+}
+
+// abs returns a copy vector with applied Abs() function to every it's value
+func (v Vector) abs() Vector {
+	for index, val := range v.values {
+		v.values[index] = math.Abs(val)
+	}
+	return v
 }
