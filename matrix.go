7d734773024eb782a614d658484d2fe70c6c5e8b
doojin <dmitry.papka@gmail.com>
2014-12-12 09:52:53 -0200

    Col method added

--- matrix.go
+++ matrix.go
@@ -5,7 +5,16 @@ type Matrix struct {
 	vectors []Vector
 }
 
-// Adds vector to the slice of vecotrs
+// AddVector appends vector to the slice of vecotrs
 func (m *Matrix) AddVector(v Vector) {
 	m.vectors = append(m.vectors, v)
+}
+
+// Col return matrix column
+func (m *Matrix) Col(colIndex int) (v Vector) {
+	for _, vector := range m.vectors {
+		value := vector.Get(colIndex)
+		v.values = append(v.values, value)
+	}
+	return
 }
--- matrix_test.go
+++ matrix_test.go
@@ -2,6 +2,7 @@ package mgauss
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,4 +15,19 @@ func Test_AddVector_ShouldAppendVectorElementToTheVectorSlice(t *testing.T) {
 	m.AddVector(v)
 
 	assert.Equal(t, m.vectors, []Vector{v}, "Should be equal")
+}
+
+func Test_Col_ShouldReturnVectorOfMatrixColumnElements(t *testing.T) {
+	m := Matrix{}
+	m.vectors = []Vector{
+		Vector{[]float64{-1, 2.5, -3, 4}},
+		Vector{[]float64{5, -6, 7, -8.5}},
+		Vector{[]float64{-9, 10.5, -11, 12}},
+		Vector{[]float64{13, -14, 0, -16.5}},
+	}
+
+	assert.Equal(t, Vector{[]float64{-1, 5, -9, 13}}, m.Col(0), "Should be equal")
+	assert.Equal(t, Vector{[]float64{2.5, -6, 10.5, -14}}, m.Col(1), "Should be equal")
+	assert.Equal(t, Vector{[]float64{-3, 7, -11, 0}}, m.Col(2), "Should be equal")
+	assert.Equal(t, Vector{[]float64{4, -8.5, 12, -16.5}}, m.Col(3), "Should be equal")
 }
